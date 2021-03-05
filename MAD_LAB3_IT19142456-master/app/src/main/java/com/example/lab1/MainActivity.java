04325a7b2f926819995933602cf280e132c19ad5
IT19142456 <80092012+IT19142456@users.noreply.github.com>
2021-03-05 14:55:13 +0530

    Add files via upload

---
+++ MAD_LAB3_IT19142456-master/app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
